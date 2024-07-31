1ee42052ac1ddba77488e720d3ae0c09f67cebb1
Guilherme Rissi <102769917+glrmrissi@users.noreply.github.com>
2024-07-31 00:21:59 -0300

    Add files via upload

---
+++ assets/js/clickOverlay/click.js
@@ -0,0 +1,36 @@
+function addClickEventToImages() {
+    const images = document.querySelectorAll('.img-anime-container');
+
+    images.forEach(image => {
+        image.addEventListener('click', function () {
+            // Seleciona o contêiner do anime e, dentro dele, a descrição. 
+            const descOverlay = this.nextElementSibling;
+            console.log('Imagem clicada:', this);
+            if (descOverlay) {
+                // descOverlay.style.display = descOverlay.style.display === 'flex' ? 'none' : 'flex';
+                descOverlay.classList.add('active')
+                console.log("Descrição achada")
+            } else {
+                console.log("Não achou a descrição!")
+            }
+        });
+    });
+}
+
+// close-synopsis
+
+function closeSynopsis() {
+    const btnCloseSynopsis = document.querySelectorAll('.close-synopsis');
+    btnCloseSynopsis.forEach(btnClose => {
+        btnClose.addEventListener('click', function () {
+            const descOverlay = this.closest('.anime-container').querySelector(".desc-overlay");
+            if(btnClose) {
+                console.log('clicked in btnClose')
+                descOverlay.classList.remove('active')
+            } else {
+                console.log('Your logic doenst makes sense');
+            }
+        });
+    });
+}
+
